e75b1265bbff2a31a02df7e00436280ebcd5894f
Adam Yanalunas <adam@yanalunas.com>
2012-07-23 23:49:59 -0700

    Initial commit, no tests.

---
+++ backbone.abide.js
@@ -0,0 +1,104 @@
+(function(Backbone, _, $, undefined) {
+	"use strict";
+	
+	var defaultOptions = ['target'];
+	
+	Backbone.Abide = function(options) {
+		this.cid = _.uniqueId('view');
+		this._configure(options || {});
+		this._ensureElement();
+		this.initialize.apply(this, arguments);
+		this.delegateEvents();
+		
+		//This needs to be moved out. Can't reference app in a library
+		var self = this;
+		if(self.timeSession) {
+			self.initializeSession();
+		}
+	};
+	Backbone.Abide.extend = Backbone.View.extend;
+	
+	_.extend(Backbone.Abide.prototype, Backbone.Session.prototype, Backbone.View.prototype, Backbone.Events, {
+		disableWhileSubmitting: true,
+		finishedText: 'Submitting...',
+		submitText: 'Submitting...',
+		unloadEvents: {
+			'keydown input': '_keypress',
+			'change input': '_keypress',
+			'change select': '_keypress',
+			'change textarea': '_keypress'
+		},
+		
+		submit: function(e) {
+			var self = this;
+			
+			self.validateForm();
+			
+			self.trigger('validating', self);
+			if(self.isValid() !== true) {
+				//Use a _.deferred() here?
+				self.trigger('validationFailed');
+				
+				return false;
+			} else {
+				var $submits = this.$('[type=submit]').not('.abide-ignore');
+				if(self.disableWhileSubmitting) self._freezeButtons($submits);
+				self._setButtonText($submits, self.submitText);
+				
+				$.when.apply(null, self.promises()).done(function(val) {
+					if(self.disableWhileSubmitting) self._restoreButtons($submits);
+					self._setButtonText($submits, self.finishedText);
+					self._dirty = false;
+					self.trigger('done', e);
+				}).fail(function(jqXHR, textStatus, errorThrown) {
+					var response = $.parseJSON(jqXHR.responseText);
+					self.trigger('fail', jqXHR, textStatus, errorThrown);
+					if(self.disableWhileSubmitting) self._restoreButtons($submits);
+					self._setButtonText($submits);
+					return false;
+				});
+			}
+			
+			return true;
+		},
+		
+		renewSession: function() {
+			this._dirty = false;
+			document.location.href = this.sessionPath;
+		},
+		
+		_freezeButtons: function(buttons) {
+			_.each(buttons, function(btn) {
+				$(btn).attr('disabled', true);
+			});
+		},
+		
+		_restoreButtons: function(buttons) {
+			var self = this;
+			_.each(buttons, function(btn) {
+				$(btn).attr('disabled', null);
+			});
+		},
+		
+		_setButtonText: function(buttons, text) {
+			_.each(buttons, function(btn) {
+				var $btn = $(btn);
+				if(!$btn.data('original-text')) {
+					$btn.data('original-text', $btn.text());
+				}
+				
+				text || (text=$btn.data('original-text'));
+				$btn.text(text);
+			});
+		},
+		
+		_dirty: false,
+		_keypress: function(e) {
+			this._dirty = true;
+		},
+		
+		showError: function() {},
+		isValid: function() { return false; },
+		validateForm: function(){}
+	});
+})(Backbone, _, $);
